92c97a407e7c5d89e27aa5cde9b2e9b400a33437
Tara Sullivan <tasulliv@ucsd.edu>
2020-03-12 13:56:47 -0700

    Update

--- code/beta/from_titan/simple_beta_bernoulli.m
+++ code/beta/from_titan/simple_beta_bernoulli.m
@@ -1,3 +1,5 @@
+clc; clear all;
+
 ab_0 = [3,2; 1,1; 0,1];
 delta=0.96;
 
@@ -7,4 +9,10 @@ dtilde  = ceil(delta/(1-delta));
 
 ss_0 = ab_0;
 
-ss_0(:,1)./(1-delta)
+ss_0(:,1)./(1-delta);
+
+% Calculate Initial Index for Each Skill:
+I_00 = dtilde.*( (delta.^(dtilde-sum(ss_0,2))) ./ sum(ss_0,2) );
+I_0  = (ss_0(:,1)./(1-delta)).*I_00;
+
+ss_0
